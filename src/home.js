6fad0ab8923b7d2fc493edc258f41a33677125b4
MichalMiekina <michal.miekina98@gmail.com>
2021-12-21 09:57:15 +0100

    add maps gallery and form

--- src/home.js
+++ src/home.js
@@ -1,6 +1,28 @@
-// import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { buildMap } from './Terrain'
 
 
+
+const scene = new THREE.Scene()
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+const canvas = document.querySelector('canvas.gallery')
+
+const camera = new THREE.PerspectiveCamera(90, sizes.width / sizes.height, .1, 1000)
+scene.add(camera)
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
 
 const apiUrl = 'http://localhost:8080/api/'
 const data = {
@@ -14,14 +36,20 @@ const data = {
     "worldName": "miejskie, duze skrzyzowanie"
 }
 var token = ''
-
+var map
 document.getElementById("api-get").addEventListener(
     'click',
     function () {
         console.log("GET")
         fetch(apiUrl + "list-worlds")
             .then(response => response.json())
-            .then(data => console.log(data))
+            // .then(data => console.log(data))
+            // .then(data => map)
+            .then(data =>map = data[0])
+            .then(data => buildMap(scene))
+
+
+        console.log(map)
     }
 )
 
